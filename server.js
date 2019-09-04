85483cb8771dc7320ef7e40e9fc1a517353e10e6
JStheBest <zwang198@uiowa.edu>
2019-09-04 05:10:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
@@ -17,13 +16,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/profile', require('./routes/profile'));
 
 // Handle production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static(__dirname + '/public/'));
 
-    // Handle SPA
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-}
 
 const port = process.env.PORT || 8000;
 
